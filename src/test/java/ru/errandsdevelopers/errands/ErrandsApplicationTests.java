a67e0dadeef87d9abb39b5ec502c8cacdd59032a
krisnovitskaya <voitage@yandex.ru>
2021-03-12 08:09:59 +0300

    init

---
+++ src/test/java/ru/errandsdevelopers/errands/ErrandsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.errandsdevelopers.errands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
